2ca79be22668f4545bc7803d9bc42a33c6db85ca
fqc <337940626@qq.com>
2016-04-30 20:49:58 +0800

    Initial commit

---
+++ zookeeper_sidepro/src/main/java/com/fqc/zk/crud/ZkCrud.java
@@ -0,0 +1,99 @@
+package com.fqc.zk.crud;
+
+import org.apache.hadoop.fs.Stat;
+import org.apache.zookeeper.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by fqc on 2016/4/30.
+ */
+public class ZkCrud {
+    ZooKeeper zk;
+    private static final int CONNECTION_TIMEOUT = 30000;
+    private static final String CONNECTION_STRING = "mini2:2181";
+    Watcher wc = new Watcher() {
+
+        public void process(WatchedEvent watchedEvent) {
+            System.out.println("watching...");
+        }
+    };
+
+    @Before
+    public void init() {
+
+        try {
+            zk = new ZooKeeper(CONNECTION_STRING, CONNECTION_TIMEOUT, this.wc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void close() {
+        try {
+            zk.close();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void createZkNode() {
+        //create / "createZkNode"
+        try {
+            String resultPath = zk.create("/zkTest", "createZkNode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getZkNode() {
+        try {
+            if (zk.exists("/zkTest", this.wc) != null) {
+                byte[] data = zk.getData("/zkTest", this.wc, null);
+                System.out.println(new String(data));
+            } else {
+                System.out.println("no zkTest zkNode");
+            }
+
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void setZkNode() {
+        try {
+            zk.setData("/zkTest", new String("helloworld2").getBytes(), -1);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        };
+
+    }
+
+    @Test
+    public void deleteZkNode() {
+        try {
+            zk.delete("/zkTest", -1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
